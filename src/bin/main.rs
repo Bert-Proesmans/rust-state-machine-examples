33664bb43bb7cac14e4c2c04dfc2143170d6dfe1
Bert Proesmans <bproesmans@hotmail.com>
2018-03-24 22:44:34 +0100

    Introduced machine memory for pushdown states

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,24 +8,28 @@ fn new_machine() -> Machine<Wait<Start>> {
     Machine {
         state: PhantomData,
         transaction: Epsilon,
-        storage: StorageService { 
-        	tape: vec![]
-        },
+        storage: StorageService { tape: vec![] },
     }
 }
 
 fn main() {
     let start_state = new_machine();
 
+    // DBG; The following syntax can/will be made simpler by implementing the TransitionInto-
+    // counterpart of TransitionFrom.
     let input_state: Machine<Wait<Input>> = TransitionFrom::transition_from(start_state, Epsilon);
 
-    let action_state: Machine<Action<Print>> = PushdownFrom::pushdown_from(input_state, PrintTransaction("Hello"));
+    let action_state: Machine<Action<Print>> =
+        PushdownFrom::pushdown_from(input_state, PrintTransaction("Hello"));
 
-    let deep_action_state: Machine<Action<Load>> = PushdownFrom::pushdown_from(action_state, Epsilon);
+    let deep_action_state: Machine<Action<Load>> =
+        PushdownFrom::pushdown_from(action_state, Epsilon);
 
-    let action_state: Machine<Action<Print>> = PullupFrom::pullup_from(deep_action_state);
+    let action_state: Machine<Action<Print>> =
+        PullupFrom::pullup_from(deep_action_state).expect("Transition Error");
 
-    let input_state: Machine<Wait<Input>> = PullupFrom::pullup_from(action_state);
+    let input_state: Machine<Wait<Input>> =
+        PullupFrom::pullup_from(action_state).expect("Transition Error");
 
     let finished_state: Machine<Finished> = TransitionFrom::transition_from(input_state, Epsilon);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,12 @@
 // Linters
-#![allow(dead_code, let_and_return, unused_mut, unused_variables)]
+#![allow(dead_code, unused_mut, unused_variables, let_and_return)]
 // Unstable features
-#![feature(associated_type_defaults)]
+#![feature(associated_type_defaults, universal_impl_trait, try_from)]
 #![cfg_attr(test, feature(plugin))]
 #![cfg_attr(test, plugin(clippy))]
 
-#[macro_use]
-extern crate serde_derive;
-
-extern crate serde;
-extern crate bincode;
-
 use std::marker::PhantomData;
-//
-use serde::{Serialize, Deserialize};
+use std::convert::{TryFrom, TryInto};
 
 ////////////////
 // Interfaces //
@@ -29,16 +22,13 @@ pub trait State {
 
 /* Markers */
 pub trait Transaction {}
+pub trait TransactionContainer {}
+
 pub trait Service {}
 
 pub trait TopLevelState: State {}
 pub trait WaitableState: State {}
 pub trait ActionableState: State {}
-
-// Type which can be used as empty transaction.
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct Epsilon;
-impl Transaction for Epsilon {}
 
 /* Functionality traits */
 pub trait ServiceCompliance<S>
@@ -62,24 +52,26 @@ where
     fn transition_from(_: T, _: <Self::State as State>::Transaction) -> Self;
 }
 
-pub trait PushdownFrom<T>
+pub trait PushdownFrom<T, TTC>
 where
+    TTC: TransactionContainer + 'static,
     T: StateContainer + Sized + 'static,
-    Self: StateContainer + ServiceCompliance<StorageService> + Sized + 'static,
+    Self: StateContainer + ServiceCompliance<StorageService<TTC>> + Sized + 'static,
     Self::State: State,
-    <Self::State as State>::Transaction: Transaction + Serialize + Deserialize<'static> + Copy + 'static,
+    <Self::State as State>::Transaction: Transaction + Copy + 'static,
 {
     fn pushdown_from(_: T, _: <Self::State as State>::Transaction) -> Self;
 }
 
-pub trait PullupFrom<T>
+pub trait PullupFrom<T, TTC>
 where
-    T: StateContainer + ServiceCompliance<StorageService> + Sized + 'static,
+    TTC: TransactionContainer + 'static,
+    T: StateContainer + ServiceCompliance<StorageService<TTC>> + Sized + 'static,
     Self: Sized + StateContainer + 'static,
     Self::State: State,
-    <Self::State as State>::Transaction: Transaction + Deserialize<'static> + 'static,
+    <Self::State as State>::Transaction: Transaction + 'static,
 {
-    fn pullup_from(_: T) -> Self;
+    fn pullup_from(_: T) -> Result<Self, String>;
 }
 
 //////////////
@@ -87,21 +79,33 @@ where
 //////////////
 
 #[derive(Debug)]
-pub struct StorageService {
-	// Used encoding for the tape:
-    // [DATA-1] [LENGTH-1] [DATA-2] [LENGTH-2] ||WRITE_HEAD|| .. [EOF]
-    // This provides a reverse linked list so we can easily push and pop data.
-    pub tape: Vec<u8>
+pub struct StorageService<A>
+where
+    A: TransactionContainer,
+{
+    pub tape: Vec<A>,
 }
-impl Service for StorageService {}
+impl<A> Service for StorageService<A>
+where
+    A: TransactionContainer,
+{
+}
 
-impl StorageService {
-    pub fn write_tape<T: Serialize>(&mut self, t: T) -> bincode::Result<()> {
-        unimplemented!()
+impl<A> StorageService<A>
+where
+    A: TransactionContainer,
+{
+    pub fn write_tape<T: Into<A>>(&mut self, t: T) {
+        self.tape.push(t.into());
     }
 
-    pub fn read_tape<T: Deserialize<'static>>(&mut self) -> bincode::Result<T> {
-        unimplemented!()
+    pub fn read_tape<T>(&mut self) -> Result<T, String>
+    where
+        A: TryInto<T, Error = String>,
+    {
+        // Note; Written out explicitly because compiler couldn't infer involved types
+        let item: Result<A, String> = self.tape.pop().ok_or_else(|| "Popped too many!".into());
+        item?.try_into()
     }
 }
 
@@ -116,7 +120,7 @@ where
 {
     pub state: PhantomData<X>,
     pub transaction: X::Transaction,
-    pub storage: StorageService,
+    pub storage: StorageService<TransactionItem>,
 }
 
 impl<X> StateContainer for Machine<X>
@@ -126,15 +130,15 @@ where
     type State = X;
 }
 
-impl<X> ServiceCompliance<StorageService> for Machine<X>
+impl<X> ServiceCompliance<StorageService<TransactionItem>> for Machine<X>
 where
     X: TopLevelState,
 {
-    fn get(&self) -> &StorageService {
+    fn get(&self) -> &StorageService<TransactionItem> {
         &self.storage
     }
 
-    fn get_mut(&mut self) -> &mut StorageService {
+    fn get_mut(&mut self) -> &mut StorageService<TransactionItem> {
         &mut self.storage
     }
 }
@@ -179,13 +183,13 @@ impl WaitableState for Input {}
 
 #[derive(Debug)]
 pub struct Action<A: ActionableState>(A);
-impl<A> State for Action<A> 
+impl<A> State for Action<A>
 where
     A: ActionableState,
 {
     type Transaction = A::Transaction;
 }
-impl<A> TopLevelState for Action<A> 
+impl<A> TopLevelState for Action<A>
 where
     A: ActionableState,
 {
@@ -203,13 +207,10 @@ impl ActionableState for Load {}
 #[derive(Debug)]
 pub struct Print();
 impl State for Print {
+    // !-- See below *Transactions --!
     type Transaction = PrintTransaction;
 }
 impl ActionableState for Print {}
-
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
-pub struct PrintTransaction(pub &'static str);
-impl Transaction for PrintTransaction {}
 
 ///////////////////////
 // Toplevel FINISHED //
@@ -221,6 +222,77 @@ impl State for Finished {
     type Transaction = Epsilon;
 }
 impl TopLevelState for Finished {}
+
+//////////////////
+// Transactions //
+//////////////////
+
+#[derive(Debug)]
+pub enum TransactionItem {
+    Epsilon(Epsilon),
+    Print(PrintTransaction),
+}
+impl TransactionContainer for TransactionItem {}
+
+// Type which can be used as empty transaction.
+#[derive(Debug, Clone, Copy)]
+pub struct Epsilon;
+impl Transaction for Epsilon {}
+
+impl From<Epsilon> for TransactionItem {
+    fn from(x: Epsilon) -> Self {
+        TransactionItem::Epsilon(x)
+    }
+}
+
+impl TryFrom<TransactionItem> for Epsilon {
+    type Error = String;
+
+    fn try_from(tc: TransactionItem) -> Result<Self, Self::Error> {
+        match tc {
+            TransactionItem::Epsilon(x) => Ok(x),
+            _ => Err("Unexpected item".into()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct PrintTransaction(pub &'static str);
+impl Transaction for PrintTransaction {}
+
+impl From<PrintTransaction> for TransactionItem {
+    fn from(x: PrintTransaction) -> Self {
+        TransactionItem::Print(x)
+    }
+}
+
+impl TryFrom<TransactionItem> for PrintTransaction {
+    type Error = String;
+
+    fn try_from(tc: TransactionItem) -> Result<Self, Self::Error> {
+        match tc {
+            TransactionItem::Print(x) => Ok(x),
+            _ => Err("Unexpected item".into()),
+        }
+    }
+}
+
+/* Transaction helpers */
+pub fn pack_transaction<T, TC>(x: T) -> TC
+where
+    T: Transaction + Into<TC> + Sized + 'static,
+    TC: TransactionContainer + Sized + 'static,
+{
+    x.into()
+}
+
+pub fn unpack_transaction<T, TC>(tc: TC) -> Result<T, TC::Error>
+where
+    T: Transaction + Sized + 'static,
+    TC: TransactionContainer + TryInto<T> + Sized + 'static,
+{
+    tc.try_into()
+}
 
 ////////////////////////////////
 // Transition implementations //
@@ -251,7 +323,7 @@ impl TransitionFrom<Machine<Wait<Input>>> for Machine<Finished> {
 }
 
 /* Machine<Wait<Input>> <-> Machine<Action<Print>> */
-impl PushdownFrom<Machine<Wait<Input>>> for Machine<Action<Print>> {
+impl PushdownFrom<Machine<Wait<Input>>, TransactionItem> for Machine<Action<Print>> {
     fn pushdown_from(old: Machine<Wait<Input>>, t: <Self::State as State>::Transaction) -> Self {
         // Build new machine.
         let mut new = Machine {
@@ -262,6 +334,7 @@ impl PushdownFrom<Machine<Wait<Input>>> for Machine<Action<Print>> {
         };
 
         // TODO: Archive state of the old machine.
+        new.storage.write_tape(old.transaction);
 
         // Return new machine.
         new
@@ -269,24 +342,25 @@ impl PushdownFrom<Machine<Wait<Input>>> for Machine<Action<Print>> {
 }
 
 /* Machine<Wait<Input>> <-> Machine<Action<Print>> */
-impl PullupFrom<Machine<Action<Print>>> for Machine<Wait<Input>> {
-    fn pullup_from(mut old: Machine<Action<Print>>) -> Self {
+impl PullupFrom<Machine<Action<Print>>, TransactionItem> for Machine<Wait<Input>> {
+    fn pullup_from(mut old: Machine<Action<Print>>) -> Result<Self, String> {
         // TODO; Restore previously stored state.
         // DBG; Transaction shouldn't be declared here!
-        let old_transaction = Epsilon;
+        // let old_transaction = Epsilon;
+        let old_transaction = old.storage.read_tape()?;
 
         // Build new machine.
-        Machine {
+        Ok(Machine {
             state: PhantomData,
             transaction: old_transaction,
             // Following properties MUST stay in sync with `Machine` !
             storage: old.storage,
-        }
+        })
     }
 }
 
 /* Machine<Action<Print>> <-> Machine<Action<Load>> */
-impl PushdownFrom<Machine<Action<Print>>> for Machine<Action<Load>> {
+impl PushdownFrom<Machine<Action<Print>>, TransactionItem> for Machine<Action<Load>> {
     fn pushdown_from(old: Machine<Action<Print>>, t: <Self::State as State>::Transaction) -> Self {
         // Build new machine.
         let mut new = Machine {
@@ -297,6 +371,7 @@ impl PushdownFrom<Machine<Action<Print>>> for Machine<Action<Load>> {
         };
 
         // TODO: Archive state of the old machine.
+        new.storage.write_tape(old.transaction);
 
         // Return new machine.
         new
@@ -304,18 +379,19 @@ impl PushdownFrom<Machine<Action<Print>>> for Machine<Action<Load>> {
 }
 
 /* Machine<Action<Print>> <-> Machine<Action<Load>> */
-impl PullupFrom<Machine<Action<Load>>> for Machine<Action<Print>> {
-    fn pullup_from(mut old: Machine<Action<Load>>) -> Self {
+impl PullupFrom<Machine<Action<Load>>, TransactionItem> for Machine<Action<Print>> {
+    fn pullup_from(mut old: Machine<Action<Load>>) -> Result<Self, String> {
         // TODO; Restore previously stored state.
         // DBG; Transaction shouldn't be declared here!
-        let old_transaction = PrintTransaction("DBG");
+        // let old_transaction = PrintTransaction("DBG");
+        let old_transaction = old.storage.read_tape()?;
 
         // Build new machine.
-        Machine {
+        Ok(Machine {
             state: PhantomData,
             transaction: old_transaction,
             // Following properties MUST stay in sync with `Machine` !
             storage: old.storage,
-        }
+        })
     }
 }
